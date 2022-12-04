9ecf799e93a144690afa16100f533788c4354cf3
Alexander Beavil <alex@aeroniemi.com>
2022-12-04 16:37:05 +0000

    def Board.init

--- game/classes/Board.py
+++ game/classes/Board.py
@@ -1,6 +1,16 @@
 class Board ():
-    def __init__():
-        return
+    def __init__(self, width, height):
+        """
+        @width: int, number of boxes in X axis
+        @height: int, number of boxes in Y axis
+        """
+        if not (type(width) is int and type(height) is int):
+            raise TypeError("Board dimensions should be int")
+        if width < 2 or height < 2:
+            raise Exception("Board size must be greater than 3x3")
+        self.width = width
+        self.height = height
+        self.build()
 
     def print():
         print("hello")
