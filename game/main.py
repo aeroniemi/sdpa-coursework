c556255c971eb86c5c2641f0203877f6be6b8c51
Alexander Beavil <alex@aeroniemi.com>
2022-12-05 12:17:00 +0000

    test adding lines

--- game/main.py
+++ game/main.py
@@ -2,6 +2,8 @@ from classes.Board import Board
 
 
 board = Board(3, 3)
-board.addLine((0, 0), (0, 1), "@")
+board.addLine(0, 0, 0, 1, "@")
+board.addLine(0, 1, 0, 2, "@")
+board.addLine(0, 0, 1, 0, "@")
 # board.addLine((1, 1), (1, 2), "&")
 board.draw()
