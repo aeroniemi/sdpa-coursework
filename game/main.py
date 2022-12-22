7707c20c528ab08f839a1777824b3d326fc29d8e
Alexander Beavil <alex@aeroniemi.com>
2022-12-22 21:16:14 +0000

    now i can add demo moves

--- game/main.py
+++ game/main.py
@@ -2,7 +2,6 @@ from classes.Board import Board
 from classes.Player import HumanPlayer, RandomPlayer
 
 
-board = Board(3, 3, HumanPlayer("A"), RandomPlayer("B"))
 # board.addLine(0, 0, 0, 1, "@")
 # board.addLine(0, 1, 0, 2, "@")
 # board.addLine(0, 0, 1, 0, "@")
