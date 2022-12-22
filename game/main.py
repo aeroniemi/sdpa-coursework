140fece73011e6222c02234952a812f7632c975e
Alexander Beavil <alex@aeroniemi.com>
2022-12-22 22:05:19 +0000

    docs for main

--- game/main.py
+++ game/main.py
@@ -5,11 +5,22 @@ Alex (Lexi) Beavil 2022
 
 Imports classes and launches the game
 """
+# imports
 from classes.Board import Board
-from classes.Player import HumanPlayer, RandomPlayer, ComputerPlayer
 
-a = HumanPlayer("A")
-board = Board(3, 3, a, ComputerPlayer("B"))
+"""
+The usual way of launching the game, including the initialisation routine
+"""
+board = Board(3, 3)
+board.initialise()
+
+"""
+For testing purposes a different launch method exists that bypasses settings, and allows you to add lines as-you-wish.
+Useful for easily reproducing test scenarios
+"""
+# from classes.Player import HumanPlayer, RandomPlayer, ComputerPlayer
+# a = HumanPlayer("A")
+# board = Board(3, 3, a, ComputerPlayer("B"))
 # board.addLine(1, 1, 1, 0, a)
 # board.addLine(0, 0, 1, 0, a)
 # board.addLine(0, 1, 1, 1, a)
