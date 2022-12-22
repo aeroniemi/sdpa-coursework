169dbba469d3413cd802cd80ad7652c4124fe4b4
Alexander Beavil <alex@aeroniemi.com>
2022-12-22 21:16:24 +0000

    Update main.py

--- game/main.py
+++ game/main.py
@@ -1,8 +1,9 @@
 from classes.Board import Board
-from classes.Player import HumanPlayer, RandomPlayer
+from classes.Player import HumanPlayer, RandomPlayer, ComputerPlayer
 
-
-# board.addLine(0, 0, 0, 1, "@")
-# board.addLine(0, 1, 0, 2, "@")
-# board.addLine(0, 0, 1, 0, "@")
+a = HumanPlayer("A")
+board = Board(3, 3, a, ComputerPlayer("B"))
+# board.addLine(1, 1, 1, 0, a)
+# board.addLine(0, 0, 1, 0, a)
+# board.addLine(0, 1, 1, 1, a)
 board.play()
